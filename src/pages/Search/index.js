000b43e1d2ef6ede2c29f510d21bbfd3e005c791
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-07-07 18:12:59 -0300

    Search page layout

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
+import {AreaInput, Container, Input, List} from './styles';
+import Feather from 'react-native-vector-icons/Feather';
 
 export default function Search() {
+  const [input, setInput] = useState('');
+  const [users, setUsers] = useState([]);
+
   return (
-    <View>
-      <Text>Tela Search</Text>
-    </View>
+    <Container>
+      <AreaInput>
+        <Feather name="search" size={20} color="#E52246" />
+        <Input
+          placeholder="Procurando alguem?"
+          value={input}
+          onChangeText={text => setInput(text)}
+          placeholderTextColor="#353840"
+        />
+      </AreaInput>
+    </Container>
   );
 }
