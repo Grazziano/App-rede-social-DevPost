b93bf5a382ade33874e0f6002421895aeeb117c4
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-07-08 21:32:31 -0300

    Modal profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,5 @@
 import React, {useContext, useState} from 'react';
+import {Modal, Platform} from 'react-native';
 
 import {AuthContext} from '../../contexts/auth';
 
@@ -6,22 +7,32 @@ import Header from '../../components/Header';
 import {
   Avatar,
   Button,
+  ButtonBack,
   ButtonText,
   Container,
   Email,
+  Input,
+  ModalContainer,
   Name,
   UploadButton,
   UploadText,
 } from './styles';
+
+import Feather from 'react-native-vector-icons/Feather';
 
 export default function Profile() {
   const {signOut, user} = useContext(AuthContext);
 
   const [nome, setNome] = useState(user?.nome);
   const [url, setUrl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   async function handleSignOut() {
     await signOut();
+  }
+
+  async function updateProfile() {
+    alert('TESTE');
   }
 
   return (
@@ -42,13 +53,32 @@ export default function Profile() {
       <Name>{user?.nome}</Name>
       <Email>{user?.email}</Email>
 
-      <Button bg="#428cfd">
+      <Button bg="#428cfd" onPress={() => setOpen(true)}>
         <ButtonText color="#FFF">Atualizar Perfil</ButtonText>
       </Button>
 
       <Button bg="#DDD" onPress={handleSignOut}>
         <ButtonText color="#353840">Sair</ButtonText>
       </Button>
+
+      <Modal visible={open} animationType="slide" transparent={true}>
+        <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+          <ButtonBack onPress={() => setOpen(false)}>
+            <Feather name="arrow-left" size={22} color="#121212" />
+            <ButtonText color="#121212">Voltar</ButtonText>
+          </ButtonBack>
+
+          <Input
+            placeholder={user?.nome}
+            value={nome}
+            onChangeText={text => setNome(text)}
+          />
+
+          <Button bg="#428CFD" onPress={updateProfile}>
+            <ButtonText color="#FFF">Salvar</ButtonText>
+          </Button>
+        </ModalContainer>
+      </Modal>
     </Container>
   );
 }
